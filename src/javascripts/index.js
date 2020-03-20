75e467df61b5a837f96002a7741102336c05a141
mthwetacoder <54649998+mthwetacoder@users.noreply.github.com>
2020-03-20 16:32:11 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,4 +4,10 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
+import "bootstrap"
+
+let f = document.getElementById("myForm");
+f.onsubmit = function displayMessage() {
+    let n = document.getElementById("Name").value;
+    alert("Welcome " + n + "!")
+}
